440c3ab53f36737c3e763683a1ca521917dbede4
Taras Sidak <tarasidak1990@gmail.com>
2020-11-19 16:42:58 +0200

    Updates

---
+++ precache-manifest.e1f600dfb1d26b09728fbb1e32095a59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a7317fe025d456ec8dce08afb45dfb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6084c478bb5cb875d3ef",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "2589060693bcbca9eaa5",
+    "url": "./static/css/main.d6a752ae.chunk.css"
+  },
+  {
+    "revision": "6084c478bb5cb875d3ef",
+    "url": "./static/js/2.dcf92dbb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dcf92dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2589060693bcbca9eaa5",
+    "url": "./static/js/main.e9425e5d.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.e1f600dfb1d26b09728fbb1e32095a59.js"
 );
 
 self.addEventListener('message', (event) => {
